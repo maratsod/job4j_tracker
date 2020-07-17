936616cd983165374097abc08d3aa26b2e9902c8
maratsod <sodnopov@list.ru>
2020-07-18 02:51:20 +0800

    1.7. Состояние объекта [#310858]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int capacity) {
+        this.load = capacity;
+    }
+    public void exchange(Battery another) {
+        another.load = this.load + another.load;
+        this.load = 0;
+    }
+    public static void main(String[] args) {
+        Battery first = new Battery(20);
+        Battery second = new Battery(10);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        second.exchange(first);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+
+    }
+}
