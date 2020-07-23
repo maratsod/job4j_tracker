e1dbbbda2cf396ddbef8f3f45f94345af6e92f48
maratsod <sodnopov@list.ru>
2020-07-24 03:24:17 +0800

    2. Перегрузка конструктора [#310876]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,17 +1,22 @@
 package ru.job4j.tracker;
 
 public class Item {
-    public String id;
-    public String name;
+     String name;
+     String id;
+
+    public Item() {
+    }
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    public Item(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public static void main(String[] args) {
         Item item = new Item();
     }
-    public Item() {
-    }
-    public Item(String id) {
-    }
-    public Item(String id, String name) {
-    }
-
 }
