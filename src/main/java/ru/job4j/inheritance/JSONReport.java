f7a7328e2ed435fbe173be6ceb9138ec5432e0db
maratsod <sodnopov@list.ru>
2020-07-23 23:20:50 +0800

    4. Переопределение [#310877]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("name", "body");
+        System.out.println(text);
+    }
+
+    public String generate(String name, String body) {
+        return name + " : " + "name" + System.lineSeparator() + body + " : " + "body";
+    }
+}
