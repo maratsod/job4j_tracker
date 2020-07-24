2545871dafad3f32c8cac6ac38f1d72c6c2964e8
maratsod <sodnopov@list.ru>
2020-07-24 14:39:09 +0800

    6. Сдача в кофе машине [#310847]

---
+++ src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,22 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int balance = money - price;
+
+            for(int i = 0; i < COINS.length; i++) {
+                while(balance >= COINS[i]) {
+                    balance -= COINS[i];
+                    rsl[i] = COINS[i];
+                    size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
