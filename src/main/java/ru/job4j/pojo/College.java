7749e07fbd37243664450c1fb9aab3da4eb1ecc6
maratsod <sodnopov@list.ru>
2020-07-25 21:18:39 +0800

    2. Модель данных. [#310884]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Ivan");
+        student.setSurname("Ivanov");
+        student.setPatronymic("Ivanovich");
+        student.setGroup("12345");
+        student.setEntered("01.09.2020");
+
+        System.out.println(student.getSurname() + " " + student.getName() + " " + student.getPatronymic() + ", group number is " + student.getGroup() + ", the date of entrance is " + student.getEntered());
+    }
+}
