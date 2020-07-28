b22d2d05453b8ea102b8a97a19e116d0a95665c5
maratsod <sodnopov@list.ru>
2020-07-28 14:23:23 +0800

    6. Метод замены заявки. Tracker.replace [#310888]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -5,13 +5,32 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
+
     @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
+    public void whenReplace() {
         Tracker tracker = new Tracker();
-        Item item = new Item();
-        item.setName("test1");
-        tracker.add(item);
-        Item result = tracker.findById(item.getId());
-        assertThat(result.getName(), is(item.getName()));
+        Item bug = new Item(10, "car");
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bugWithDesc = new Item(10, "dog");
+        bugWithDesc.setName("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
+    }
+
+    public void testAdd() {
+    }
+
+    public void testFindAll() {
+    }
+
+    public void testFindByName() {
+    }
+
+    public void testFindById() {
+    }
+
+    public void testReplace() {
     }
 }
