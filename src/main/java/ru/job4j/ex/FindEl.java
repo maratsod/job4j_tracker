00a32e95183dd140e2a87a66beccea7265a8c909
maratsod <sodnopov@list.ru>
2020-10-07 00:10:55 +0800

    0.4. Пользовательские исключения. [#310840]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,23 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (String x : value) {
+            if (x != key){
+                throw new ElementNotFoundException("key is not found");
+            }
+            rsl++;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] colours = { "Red", "Orange", "Yellow", "Green" };
+            indexOf(colours , "b");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
