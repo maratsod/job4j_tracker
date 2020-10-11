e310b4b43ac8d57019029ac3226327fc1c1369bf
maratsod <sodnopov@list.ru>
2020-10-11 20:15:15 +0800

    0.4. Пользовательские исключения. [#310840]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,10 +4,11 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if ( value[i] == key ) {
+            if ( value[i].equals(key)) {
                 System.out.println(key + " is found at index " + i);
             }
-            rsl++;
+            rsl = i;
+            break;
     }
         if (rsl == -1) {
             throw new ElementNotFoundException("item is not found");
@@ -18,7 +19,7 @@ public class FindEl {
     public static void main(String[] args) {
         try {
             String[] colours = { "Red", "Orange", "Yellow", "Green" };
-            indexOf(colours , "v");
+            indexOf(colours , "b");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
