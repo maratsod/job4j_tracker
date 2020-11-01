41fd8041dd963d53eb2a9b4e5f48d7d8bbeb1f2c
maratsod <sodnopov@list.ru>
2020-11-01 23:05:30 +0800

    3.2. За нужно приведение типов [#310828]

---
+++ src/main/java/ru/job4j/poly/PolyUsage.java
@@ -0,0 +1,14 @@
+package ru.job4j.poly;
+
+public class PolyUsage {
+    public static void main(String[] args) {
+        Vehicle plane= new Plane();
+        Vehicle train = new Train();
+        Vehicle ship = new Ship();
+
+        Vehicle[] vehicles = new Vehicle[]{plane, train, ship};
+        for (Vehicle a : vehicles) {
+            a.move();
+        }
+    }
+}
