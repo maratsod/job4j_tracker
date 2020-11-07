671e816a3f2f5c2a5ae181a92fb4b0d4f96f661c
maratsod <sodnopov@list.ru>
2020-11-07 23:30:28 +0800

    2. Идентичные тексты. [#310811]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String element : origin) {
+            check.add(element);
+        }
+            for (String elementText : text) {
+                if (!check.contains(elementText)) {
+                    rsl = false;
+                }
+        }
+        return rsl;
+    }
+}
