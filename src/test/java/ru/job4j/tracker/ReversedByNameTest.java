e48906d589191245374c8665e0037acf6da24a16
maratsod <sodnopov@list.ru>
2021-01-11 21:17:12 +0800

    0. Сортировка [#310795]

---
+++ src/test/java/ru/job4j/tracker/ReversedByNameTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.collection.ConvertList;
+import static org.hamcrest.Matchers.greaterThan;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+    public class ReversedByNameTest {
+        @Test
+        public void whenSortByName() {
+            int rsl = new Item(32, "Petr")
+                    .compareTo(
+                            new Item(31,"Ivan")
+                    );
+            assertThat(rsl, greaterThan(0));
+        }
+    }
---
+++ src/test/java/ru/job4j/tracker/SortByNameTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+
+import org.junit.Test;
+import ru.job4j.collection.ConvertList;
+import static org.hamcrest.Matchers.greaterThan;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SortByNameTest {
+            @Test
+            public void whenSortByName() {
+                int rsl = new Item(32, "Petr")
+                        .compareTo(
+                                new Item(31,"Ivan")
+                        );
+                assertThat(rsl, greaterThan(0));
+            }
+    }
