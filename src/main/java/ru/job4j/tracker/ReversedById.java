f706243e209acd60389e0ad8e5b02a6d437334a0
maratsod <sodnopov@list.ru>
2021-01-11 19:10:50 +0800

    0. Сортировка [#310795]

--- src/main/java/ru/job4j/tracker/ReversedById.java
+++ src/main/java/ru/job4j/tracker/ReversedById.java
@@ -5,14 +5,6 @@ import java.util.Comparator;
 public class ReversedById implements Comparator<Item>{
     @Override
     public int compare(Item first, Item second) {
-        if (second.getId() > first.getId()) {
-            return 1;
-        }
-        else if (second.getId() < first.getId()) {
-            return -1;
-        }
-        else {
-            return 0;
+        return Integer.compare(second.getId(), first.getId());
         }
     }
-}
