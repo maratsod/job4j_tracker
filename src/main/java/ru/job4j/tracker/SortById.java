df6eb5003c84521d9989bfab4b6cb3fb4870fb40
maratsod <sodnopov@list.ru>
2021-01-11 19:16:50 +0800

    0. Сортировка [#310795]

--- src/main/java/ru/job4j/tracker/SortById.java
+++ src/main/java/ru/job4j/tracker/SortById.java
@@ -5,14 +5,6 @@ import java.util.Comparator;
 public class SortById implements Comparator<Item> {
     @Override
     public int compare(Item first, Item second) {
-        if (first.getId() > second.getId()) {
-            return 1;
-        }
-        else if (first.getId() < second.getId()) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
+        return Integer.compare(first.getId(), second.getId());
     }
 }
