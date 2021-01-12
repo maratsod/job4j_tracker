ceacb70a447839b85f816095b2e37881f498aa14
maratsod <sodnopov@list.ru>
2021-01-13 03:25:12 +0800

    2. Комбинированный компаратор [#310794]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] arrayLeft = left.split(".");
+        String[] arrayRight = right.split(".");
+        int a = 0;
+        int b = 0;
+        for (String s : arrayLeft) {
+             a = Integer.parseInt(arrayLeft[0]);
+        }
+        for (String s : arrayRight) {
+             b = Integer.parseInt(arrayRight[0]);
+        }
+        return Integer.compare(a, b);
+    }
+}
