38beb5875ec2e4b926cf5999717038712e989e52
maratsod <sodnopov@list.ru>
2021-01-13 02:20:48 +0800

    3. Компаратор для строк [#310791]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -7,33 +7,16 @@ public class StringCompare implements Comparator<String>{
     public int compare(String left, String right) {
         int rsl = 0;
         int size = Math.min(left.length(), right.length());
-        if (left.length() < right.length()) {
-             rsl = -1;
-                for (int i = 0; i < size; i++) {
-                    if (left.charAt(i) != right.charAt(i)) {
-                        rsl = Character.compare(left.charAt(i), right.charAt(i));
-                        break;
-                    }
-                }}
+        for (int i = 0; i < size; i++) {
+           if (left.charAt(i) != right.charAt(i)) {
+               rsl = Character.compare(left.charAt(i), right.charAt(i));
+           }
 
-        if (left.length() > right.length()) {
-            rsl = -1;
-            for (int i = 0; i < size; i++) {
-                if (left.charAt(i) != right.charAt(i)) {
-                    rsl = Character.compare(left.charAt(i), right.charAt(i));
-                    break;
-                }
-            }}
-
-                if (left.length() == right.length()) {
-                    for (int i = 0; i < size; i++) {
-                        if (left.charAt(i) != right.charAt(i)) {
-                            rsl = Character.compare(left.charAt(i), right.charAt(i));
-                            break;
-                        }
-                    }
-                }
-
+        }
+        if (rsl == 0) {
+            rsl = Integer.compare(left.length(), right.length());
+        }
                 return rsl;
             }
+
      }
