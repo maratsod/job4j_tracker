ba398f8134e51a06f545e452f4380b8f57748080
maratsod <sodnopov@list.ru>
2021-01-16 16:36:25 +0800

    2.2. Лямбда блок [#310642]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Collections;
+
+public class FI {
+    public static void main(String[] args) {
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("compare - " + left + " : " + right);
+            return right.compareTo(left);
+        };
+        List<String> list = Arrays.asList("January", "February", "March", "April", "May");
+        System.out.println(list + " ");
+        Collections.sort(list, comparator);
+        for(String str : list) {
+            System.out.print(str + " ");
+        }
+    }
+}
