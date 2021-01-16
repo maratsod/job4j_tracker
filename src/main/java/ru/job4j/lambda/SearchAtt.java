0c439a67a7e50ef38b90062a034db23a0ba3f002
maratsod <sodnopov@list.ru>
2021-01-16 15:24:46 +0800

    2.0. Встроенные функциональные интерфейсы. [#310643]

---
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -0,0 +1,51 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.*;
+
+public class SearchAtt {
+
+    public static List<Attachment> filterSize(List<Attachment> list, int value) {
+        Predicate<Attachment> predicate = new Predicate<>() {
+            @Override
+            public boolean test(Attachment att){
+                return att.getSize() > value;
+            }
+        };
+        return loop(list, predicate);
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list, String value) {
+        Predicate<Attachment> predicate = new Predicate<>() {
+            @Override
+            public boolean test(Attachment att){
+                return att.getName().contains(value);
+            }
+        };
+        return loop(list, predicate);
+    }
+
+    private static List<Attachment> loop(List<Attachment> list, Predicate<Attachment> predicate) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (predicate.test(att)) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image1", 110),
+                new Attachment("image2", 14),
+                new Attachment("image3", 1193)
+        );
+        List<Attachment> compare = filterSize(attachments, 100);
+            for(Attachment newAtt : compare) {
+                System.out.println(newAtt);
+            }
+        }
+}
