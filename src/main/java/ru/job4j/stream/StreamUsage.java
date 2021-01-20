03118776d0179d9362ca88d1c20ea578be77a7da
maratsod <sodnopov@list.ru>
2021-01-20 15:16:04 +0800

    0.1. Отладка лямбда в IDEA [#310656]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,12 +1,11 @@
 package ru.job4j.stream;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamUsage {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(
+        List<Integer> list = List.of(
                 -1, 2, -3, 4, -5, 6, -7, 8, -9, 10
         );
         list = list.stream().filter(
