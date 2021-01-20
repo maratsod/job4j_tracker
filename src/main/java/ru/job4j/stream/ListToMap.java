05305551353e527a4a4ee799231616a1b86d9f3c
maratsod <sodnopov@list.ru>
2021-01-20 17:18:29 +0800

    4. Преобразование List в Map. [#310653]

---
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListToMap {
+    public static Map<String, Student> listToMap(List<Student> students) {
+        return students.stream()
+                .distinct()
+                .collect(Collectors.toMap(
+                        Student :: getSurname,
+                        e -> e
+                        ));
+    }
+}
