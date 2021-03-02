904bc0dcc89da89540b264e43229d4f12943823c
maratsod <sodnopov@list.ru>
2021-03-02 19:38:03 +0800

    1. Анонимные классы [#310640]

--- src/main/java/ru/job4j/lambda/Attachment.java
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -19,9 +19,10 @@ public class Attachment {
 
     @Override
     public String toString() {
-        return "{" +
-                "name='" + name + '\'' +
-                ", size=" + size +
-                '}';
+        return "{"
+                + "name='" + name
+                + '\''
+                + ", size=" + size
+                + '}';
     }
 }
