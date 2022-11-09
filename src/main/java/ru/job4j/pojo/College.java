73509655d031b53be02105469399c593181f393d
marat92 <sodnopov@list.ru>
2022-11-09 19:20:16 +0700

    2. Модель данных. [#174683 #35000]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Q W E");
+        student.setGroup(1320);
+        student.setDate("12.12.2022");
+        System.out.println(student);
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,40 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String name;
+    private int group;
+    private String date;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Student's " +
+                "name is '" + name + '\'' +
+                ", group number is " + group +
+                ", date entered: '" + date;
+    }
+}
