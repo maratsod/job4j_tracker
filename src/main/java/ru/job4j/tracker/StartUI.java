6b9fa1ce36404fdda8fa9ed41d69c603af37635d
marat92 <sodnopov@list.ru>
2022-11-09 14:57:40 +0700

    8. Date. Отображение даты. [#310017 #34981]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime dateTime = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String time = dateTime.format(formatter);
+        System.out.println(time);
+    }
+}
