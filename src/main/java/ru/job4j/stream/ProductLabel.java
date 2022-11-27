616b861d1b17f3445964307381dcc1602fb84455
marat92 <sodnopov@list.ru>
2022-11-27 14:44:35 +0700

    4. Перенос методов в Stream API

--- src/main/java/ru/job4j/stream/ProductLabel.java
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ProductLabel {
-
     public List<String> generateLabels(List<Product> products) {
         return products.stream()
                 .filter(s -> s.getStandard() - s.getActual() >= 0)
